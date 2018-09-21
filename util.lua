103ce8d404a42e0ce134d3f88e0cf40a6ff1579a
xiongyi <xiongyi@wise-tech.com.cn>
2018-09-21 11:01:37 +0800

    1. Fix file name pattern

--- util.lua
+++ util.lua
@@ -4,16 +4,16 @@ function exec(command)
     t:close()
 end
 
-
+local patten =  "[0-9/]+%s+[0-9:]+%s+[0-9,%w<>]+%s+([%w%-%(%) _%.]+)%.%w+"
 function get_file_names(folder, filter)
     local files = {}
     local t = io.popen('dir "'.. folder .. '\\' ..filter .. '"',"r")
     if t then 
         local r = t:read("*a")
-        local p1,p2,f = string.find(r, "[0-9/]+%s+[0-9:]+%s+[0-9,]+%s+([%w _]+)%.%w+")
+        local p1,p2,f = string.find(r, patten)
         while p1 do
             files[#files+1] = f
-            p1,p2,f = string.find(r, "[0-9/]+%s+[0-9:]+%s+[0-9,]+%s+([%w _]+)%.%w+",p2+1)
+            p1,p2,f = string.find(r, patten,p2+1)
         end
         t:close()
     end
